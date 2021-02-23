231fdb988153da2e58043b2e834f823f4bb5f890
Tango <tango.tew@outlook.com>
2021-02-23 13:08:56 -0600

    clean up the output for authentication error

--- src/common-ware/index.js
+++ src/common-ware/index.js
@@ -10,7 +10,7 @@ exports.requireSignin = (req, res, next) => {
       next();
     }
     catch(err){
-      res.status(403).json("You've been signed out, Please, signin first.")
+      res.status(403).json("You've been signed out, Please signin first.")
     }
   }
   else {
